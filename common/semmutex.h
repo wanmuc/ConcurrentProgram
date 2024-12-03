af1ea16918eee7b3d25c5b96656121cde0f542b7
wanmuc <769535441@qq.com>
2024-12-03 13:10:01 +0800

    代码归档

--- common/semmutex.h
+++ common/semmutex.h
@@ -6,11 +6,19 @@
 
 class SemMutex {
  public:
-  SemMutex(std::string sem_name) : sem_name_(sem_name) {}
-  ~SemMutex() {}
+  SemMutex(std::string sem_name) : sem_name_(sem_name) {
+    sem_ = sem_open(sem_name_, O_CREAT, 0644, 1);
+    assert(sem_ != SEM_FAILED);
+  }
+  ~SemMutex() { sem_close(sem_); }
 
-  void Lock() {}
-  void UnLock() {}
+  void Lock() {
+    sem_wait(sem_);  // P 操作
+  }
+  void UnLock() {
+    sem_post(sem_);  //  V 操作
+  }
+  void Delete() { sem_unlink(sem_name_); }  // 删除信号量
 
  private:
   sem_t *sem_;
